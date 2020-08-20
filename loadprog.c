2c15a6e4bf4948401fd70015dd24be0db39f12c5
primal-coding <primal.coding@gmail.com>
2020-08-20 16:36:29 +0100

    more parsing done. added arguments structure

--- loadprog.c
+++ loadprog.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h>       // for strcpy() & strcmp()
 #include <errno.h>
 #include <ctype.h>        // for toupper()
 
@@ -9,7 +9,7 @@ extern char *filename;
 
 enum operands {MOVE, STORE, LOAD, INPUT, OUTPUT, ADD, XOR, TEST,
   JUMPEQ, JUMPNEQ, JUMP, EXIT};
-enum opcodes {CONST, R1, R2, R3, R4, R5, R6, R7, R8, R9, R0, NOOP};
+enum opcodes {R0, R1, R2, R3, R4, R5, R6, R7, R8, R9, CONST, NO_OP};
 /* structure of linked list for the lines of instructions coming
  * from the source file */
 struct node {
@@ -17,27 +17,32 @@ struct node {
   int length;
   struct node *next;
 };
+struct args {
+  enum opcodes op;
+  unsigned char value;
+};
 // linked list with the instructions parsed
 struct inst_line {
-  unsigned char operand;
-  unsigned char opcode1;
-  unsigned char opcode2;
-  struct inst *next;
+  enum operands operand;
+  struct args op1;
+  struct args op2;
+  struct inst_line *next;
 };
 
 // function definitions
 struct node *loadfile(FILE *prog, int *lines);
-void parselines(struct node *firstn);
-void parseline(char *line, int length);
+struct inst_line *parselines(struct node *firstn);
+void parseline(struct inst_line *nodei, struct node *noden);
 void free_nodes(struct node *first);
 int parse_inst(char *buf, int l);
-int parse_arg(char *buf, int l);
+struct args parse_arg(char *buf, int l);
 
 
 int openfile()
 {
   FILE *prog;
   struct node *firstn;
+  struct inst_line *firsti;
   int l = 0;
   int *lines;   // in order to not use a global variable
   lines = &l;
@@ -48,9 +53,9 @@ int openfile()
     printf("\nError loading file. Please try again.\n");
     return (1);
   }
-  firstn = loadfile(prog, lines);  // creation of the linked list
+  firstn = loadfile(prog, lines);  // load lines into a the linked list
   fclose(prog);
-  parselines(firstn);
+  firsti = parselines(firstn);   //
 
   return (0);
 }
@@ -126,35 +131,58 @@ struct node *loadfile(FILE *prog,int *lines)
   return(firstn);
 }
 
-void parselines(struct node *firstn)
+/* linked list of lines => linked list of structured data
+ * (extracting the instruction and the optional arguments
+ * in order to reorganise them to put them in another linked list) */
+struct inst_line *parselines(struct node *firstn)
 {
   struct node *indexn;
+  struct inst_line *firsti,*indexi, *newi;
   int loop = 0;
-  char *buff;
 
+  /* setting up a new linked list with the instruction
+   * and the optional arguments */
+  firsti = (struct inst_line *)malloc(sizeof(struct inst_line));
+  indexi = firsti;
+  /* looping through each line to parse for instructions and args */
   do
   {
     indexn = firstn;
-    buff = indexn->instruction;
-    parseline(buff,indexn->length);
+    parseline(indexi, indexn);
+    // checking for the end of the list (to end the loop)
     if (indexn->next == NULL)
+    {
+      free(indexn);
       loop = 1;
-    else
+      continue;
+    }
+    else    // if not the end then getting the next line to process
       firstn = indexn->next;
-    free(indexn);
+    free(indexn);     // the line for the source is not needed anymore
+    /* allocating a new struct to store instruction & args from
+     * the next iteration of the loop */
+    newi = (struct inst_line *)malloc(sizeof(struct inst_line));
+    indexi->next = newi;
+    indexi = newi;
   }
   while(!loop);
-
+  return (firsti);
 }
 
-void parseline(char *line, int length)
+void parseline(struct inst_line *nodei, struct node *noden)
 {
-  char buff[10];
-  struct inst_line *firsti,*indexi, *newi;
-  enum operands op;
-  enum opcodes op1, op2;
+  char buff[10];    // buffer for instruction or argument
+  char *line;       // line to be parsed
+  int length;
   int c, w=0, wc=0;
+  enum operands op;
+  struct args a1, a2;
+  a1.op = NO_OP;
+  a2.op = NO_OP;
 
+
+  line = noden->instruction;
+  length = noden->length;
   // iterating through each character of the line
   for (c=0;c<length-1;c++)
   {
@@ -166,10 +194,10 @@ void parseline(char *line, int length)
         op = parse_inst(buff,c-1);
         break;
         case 1:
-        op1 = parse_arg(buff,c-wc-1);
+        a1 = parse_arg(buff,c-wc-1);
         break;
         case 2:
-        op2 = parse_arg(buff,c-wc-1);
+        a2 = parse_arg(buff,c-wc-1);
         break;
         default:
         // NOP
@@ -178,11 +206,12 @@ void parseline(char *line, int length)
       w++;      // position of instruction/argument in the line
       wc = c;   // # of characters to substract for next instruct/arg
     }
-    if (line[c] != ',')
+    if (line[c] != ',')     // no need to store commas in the buffer
       buff[c-wc] = line[c];
-
-
   }
+  nodei->operand = op;
+  nodei->op1 = a1;
+  nodei->op2 = a2;
 }
 
 
@@ -234,30 +263,23 @@ int parse_inst(char *buf, int l)
   return (ret);
 }
 
-int parse_arg(char *buf, int l)
+struct args parse_arg(char *buf, int l)
 {
-  enum opcodes ret;
-  char b[10];
-  char *bb;
-  bb = b;
-  char c;
-  c = toupper(*buf);
+  struct args ret;
 
-  if (l == 8 && c == 'C')
+  if (*buf == 'R' && !(*(buf + 1)<'0' || *(buf + 1)>'9'))
   {
-    bb = buf;
-    b = bb;
-    b[8] = '\0';
-    if (strcmp(b,"CONSTANT") == 0)
-      return (CONST);
-    else
-    {
+    ret.op = *(buf + 1) - '0';
+    ret.value = 0;
+    return(ret);
+  }
+  if (*buf<'0' ||  *buf>'9')
+  {
       printf("Parsing error - Invalid argument.");
       exit(4);
-    }
   }
-
-
+  ret.op = CONST;
+  ret.value = *(buf + 1) - '0';
   return(ret);
 }
 
